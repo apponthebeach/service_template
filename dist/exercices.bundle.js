27c2dae6912ccf4067d7816d884d3646a712a1a1
Axel de Sainte Marie <appnotie@gmail.com>
2018-01-15 17:39:35 +0100

    test avec toutes les infos utilisateurs 3

--- dist/exercices.bundle.js
+++ dist/exercices.bundle.js
@@ -17882,8 +17882,8 @@ function getUserInfo(data_) {
         Workwell.getUserInfo({
             success: function (res) {
                 window.localStorage.userEmail = res.user.email; 
-                window.localStorage.userFirstName = red.user.first_name;
-                window.localStorage.userLastName = red.user.last_name;
+                window.localStorage.userFirstName = res.user.first_name;
+                window.localStorage.userLastName = res.user.last_name;
                 resolve(res);
             },
             error: function (data) {
@@ -17911,8 +17911,7 @@ function renderUI() {
         if (window.localStorage.userEmail === "") {
             data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname=AXEL&contactName=de%20Sainte%20Marie&contactEmail=axeldesaintemarie@gmail.com&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
         } else {
-            //data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname='+window.localStorage.userFirstName+'&contactName='+window.localStorage.userLastName+'&contactEmail='+window.localStorage.userEmail+'&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
-            data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname=AXEL&contactName=de%20Sainte%20Marie&contactEmail=axeldesaintemarie@gmail.com&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
+            data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname='+window.localStorage.userFirstName+'&contactName='+window.localStorage.userLastName+'&contactEmail='+window.localStorage.userEmail+'&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
         }
 
         $.ajax({
