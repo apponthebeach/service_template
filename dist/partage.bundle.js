d510ab71dd798968fe7b2840052943355f2c009e
Axel de Sainte Marie <appnotie@gmail.com>
2018-01-23 16:23:23 +0100

    Quelques corrections sur le formulaire

--- dist/partage.bundle.js
+++ dist/partage.bundle.js
@@ -17910,44 +17910,97 @@ function getUserInfo(data_) {
     });
 }
 
+
+// Function that validates email address through a regular expression.
+function validateEmail(sEmail) {
+    var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+    if (filter.test(sEmail)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function renderUI() {
     //UI
     var shareId = getParameterByName('sId');
     if (shareId == 1) {
         $("<label>À qui souhaitez-vous partager l’article ?</label>").appendTo("#partage_title");
-        $("<label>Texte partagé : </label><TextArea>L’appli et les services Wellogy https://workwell/article…. Téléchargez l'application Wellogy : https://play.google.com/wellogy</TextArea>").appendTo("#partage_body");
+        $("<label>Texte partagé : </label><TextArea id='email_body'>L’appli et les services Wellogy https://workwell/article…. Téléchargez l'application Wellogy : https://play.google.com/wellogy</TextArea>").appendTo("#partage_body");
         
     } else {
         $("<label>À qui souhaitez-vous partager l’application Wellogy ?</label>").appendTo("#partage_title");
-        $("<label>Texte partagé : </label><TextArea>Wellogy, l’application de Sophrologie et de bien-être qui vous accompagne au quotidien. Téléchargez l'application Wellogy : https://play.google.com/wellogy</TextArea>").appendTo("#partage_body");
+        $("<label>Texte partagé : </label><TextArea id='email_body'>Wellogy, l’application de Sophrologie et de bien-être qui vous accompagne au quotidien. Téléchargez l'application Wellogy : https://play.google.com/wellogy</TextArea>").appendTo("#partage_body");
     }
     
     let sharingCoachingButton = Workwell.ui.createButton("ENVOYER L'EMAIL");
     sharingCoachingButton.addClass("onboarding_button");
     sharingCoachingButton.onClick(function(){
         //Partage avec email
-        var data;
-        if (window.localStorage.userEmail === "") {
-            data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname=AXEL&contactName=de%20Sainte%20Marie&contactEmail=axeldesaintemarie@gmail.com&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
+        var emails = "";
+        var areEmailsValide = true;
+        
+        if (($("#first_email").val().length ==0) && ($("#second_email").val().length ==0) && ($("#third_email").val().length ==0)) {
+            areEmailsValide = false;
         } else {
-            data ='&recipientEmail=axel@wellogy.fr&companyName=LALALA&contactFirstname='+window.localStorage.userFirstName+'&contactName='+window.localStorage.userLastName+'&contactEmail='+window.localStorage.userEmail+'&contactSubject=WHAT%20A%20GREAT%20APP&contactMessage=THIS%20IS%20THE%20COOLEST%20APP%20IN%20THE%20WORLD';
-        }
-
-        $.ajax({
-	       type: "POST",
-	       url: "https://aotb.xyz/wellogy/site/inc/shareTheApp.php",
-	       data: data,
-	       success: function(msg) {
-                // Message was sent
-                if (msg == 'OK') {
-                    window.alert("Message envoyé");
-                }
-                // There was an error
-                else {
-                    window.alert("Erreur lors de l'envoie du message"); 
+           if ($("#first_email").val().length !=0) {
+                if (validateEmail($("#first_email").val()) == true) {
+                    emails = $("#first_email").val();
+                } else {
+                    areEmailsValide = false;
                 }
             }
-        });
+
+            if ($("#second_email").val().length !=0) {
+                if (validateEmail($("#second_email").val()) == true) {
+                    if (emails.length > 0) emails = emails + ",";
+                    emails = emails + $("#second_email").val();
+                } else {
+                    areEmailsValide = false;
+                }
+            }
+
+            if ($("#third_email").val().length !=0) {
+                if (validateEmail($("#third_email").val()) == true) {
+                    if (emails.length > 0) emails = emails + ",";
+                    emails = emails + $("#third_email").val();
+                } else {
+                    areEmailsValide = false;
+                }
+            } 
+        }
+        
+        if (areEmailsValide == true) {
+            if ($("#email_body").val().length > 0) {
+                var data;
+                if (window.localStorage.userEmail === "") {
+                    data ='&recipientEmail='+emails+'&companyName=&contactFirstname=Well&contactName=Ogy&contactEmail=axel@wellogy.com&contactSubject=Découvrez%20Wellogy&contactMessage='+$("#email_body").val();
+                } else {
+                    data ='&recipientEmail='+emails+'&companyName=LALALA&contactFirstname='+window.localStorage.userFirstName+'&contactName='+window.localStorage.userLastName+'&contactEmail='+window.localStorage.userEmail+'&contactSubject=Découvrez%20Wellogy&contactMessage='+$("#email_body").val();
+                }
+
+                $.ajax({
+                   type: "POST",
+                   url: "https://aotb.xyz/wellogy/site/inc/shareTheApp.php",
+                   data: data,
+                   success: function(msg) {
+                        // Message was sent
+                        if (msg == 'OK') {
+                            window.alert("Message envoyé");
+                        }
+                        // There was an error
+                        else {
+                            window.alert("Erreur lors de l'envoie du message"); 
+                        }
+                    }
+                });
+            } else {
+                window.alert("Votre message est vide");
+            }
+        }
+        else {
+            window.alert("Vérifiez le format de vos emails");
+        }
     });
     $("#partage_button").append(sharingCoachingButton.toHTMLElement());
 }
