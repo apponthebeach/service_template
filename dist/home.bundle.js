d8f8b3153fca6e46100de8dd2dd18958875da24b
Axel de Sainte Marie <appnotie@gmail.com>
2018-02-01 10:29:45 +0100

    Modification du partage

--- dist/home.bundle.js
+++ dist/home.bundle.js
@@ -17902,7 +17902,7 @@ function renderUI() {
     
     //UI
     document.getElementById("home_first_section").onclick = function() {
-        Workwell.openWebPage(window.location.href + "../../video?vId=PgVrR5lZ");
+        Workwell.openWebPage(window.location.href + "../../article?aId=1");
     };
     document.getElementById("home_second_section").onclick = function() {
         Workwell.openWebPage(window.location.href + "../../exercices");
